77d53ad6790d28241fd612cb1c7779dabbba22d5
Adam Kabak <adamkabak@yahoo.com>
2024-02-20 13:02:26 -0500

    first commit, task 1

---
+++ refined.py
@@ -0,0 +1,96 @@
+import csv
+import pandas as pd
+import ast
+import json
+
+
+def load_csv(filename):
+    """
+    Load data from a CSV file and return it as a pandas DataFrame.
+    """
+    return pd.read_csv(filename)
+
+
+def extract_tables(row):
+    """
+    Extract unique tables from the "tables" column along with their aliases.
+    """
+    tables = ast.literal_eval(row["tables"])
+    table_names = []
+    aliases = []
+    print("tables: ", tables)
+    print("aliases: ", aliases)
+    for table in tables:
+        parts = table.split(" as ")
+        table_names.append(parts[0])
+        if len(parts) > 1:
+            aliases.append(parts[1])
+        else:
+            aliases.append(None)
+    return pd.Series({"table": table_names, "aliases": [aliases]})
+
+
+def replace_table_aliases(row):
+    """
+    Replace table aliases with full table names in specified columns.
+    """
+    tables = ast.literal_eval(row["tables"])
+
+    table_name = None
+    table_alias = None
+
+    for table in tables:
+
+        parts = table.split(" as ")
+        if len(parts) == 2:
+            table_name = parts[0]
+            table_alias = parts[1]
+        else:
+            print(f"Unexpected format in 'tables' column: {table}")
+
+        if table_name is not None and table_alias is not None:
+
+            columns_to_replace = [
+                "select_columns",
+                "join_columns",
+                "where_columns",
+                "agg_columns",
+            ]
+
+            for col in columns_to_replace:
+                if isinstance(row[col], str):
+                    if pd.notna(row[col]):  # Check if the value is not NaN
+                        replaced_columns = []
+                        col_list = json.loads(row[col])
+                        for column in col_list:
+                            if column.startswith(table_alias + "."):
+                                column = column.replace(
+                                    table_alias + ".", table_name + "."
+                                )
+                            replaced_columns.append(column)
+
+                        row[col] = replaced_columns
+                elif isinstance(row[col], list):
+                    replaced_columns = []
+                    for column in row[col]:
+                        if column.startswith(table_alias + "."):
+                            column = column.replace(table_alias + ".", table_name + ".")
+                        replaced_columns.append(column)
+
+                    row[col] = replaced_columns
+
+    return row
+
+
+def main():
+
+    csv_filename = "query_analysis_data.csv"
+    data = load_csv(csv_filename)
+
+    data = data.apply(replace_table_aliases, axis=1)
+
+    return data
+
+
+if __name__ == "__main__":
+    main()
---
+++ tables_columns.py
@@ -0,0 +1,100 @@
+from refined import main as transform_data
+import pandas as pd
+import ast
+import json
+from collections import defaultdict
+
+
+def extract_columns(row):
+    """
+    Extract columns from the specified columns (select, join, where, agg) of the transformed dataset.
+    """
+    columns = []
+    columns_to_extract = [
+        "select_columns",
+        "join_columns",
+        "where_columns",
+        "agg_columns",
+    ]
+    for col in columns_to_extract:
+        if isinstance(row[col], list):
+            columns.extend([column.split(".")[-1] for column in row[col]])
+    return columns
+
+
+def unique_tables(data):
+    unique_tables = set()
+    for _, row in data.iterrows():
+        tables_list = ast.literal_eval(row["tables"])
+        for table in tables_list:
+            parts = table.split(" as ")
+            if len(parts) == 2:
+                table_name = parts[0]
+            else:
+                print(f"Unexpected format in 'tables' column: {table}")
+                table_name = table
+            unique_tables.add(table_name)
+    return unique_tables
+
+
+def generate_columns_per_table_dict(data, unique_tables):
+    columns_to_scan = [
+        "select_columns",
+        "join_columns",
+        "where_columns",
+        "agg_columns",
+    ]
+
+    table_columns = {table: set() for table in unique_tables}
+
+    for _, row in data.iterrows():
+        for table_name in unique_tables:
+            for col in columns_to_scan:
+                column_value = row[col]
+                if isinstance(column_value, float) and pd.isna(column_value):
+                    continue
+                if isinstance(column_value, str):
+                    columns = json.loads(column_value)
+                elif isinstance(column_value, list):
+                    columns = column_value
+                else:
+                    continue
+                for column in columns:
+                    if column.startswith(table_name + "."):
+                        column_name = column.split(".")[-1]
+                        table_columns[table_name].add(column_name)
+
+    return table_columns
+
+
+def create_columns_dataset(data):
+    """
+    Create a new dataset containing unique tables and their columns.
+    """
+
+    table_set = unique_tables(data)
+    table_columns = generate_columns_per_table_dict(data, table_set)
+
+    table_columns_aggregated = defaultdict(list)
+    for table, columns in table_columns.items():
+        table_columns_aggregated[table].extend(columns)
+
+    rows = []
+    for table, columns in table_columns_aggregated.items():
+        rows.append({"table": table, "columns": columns})
+
+    columns_data = pd.DataFrame(rows)
+
+    return columns_data
+
+
+def main():
+    refined_data = transform_data()
+    columns_dataset = create_columns_dataset(refined_data)
+
+    print("Columns Dataset:")
+    print(columns_dataset)
+
+
+if __name__ == "__main__":
+    main()
